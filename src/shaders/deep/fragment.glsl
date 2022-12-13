ff6834a580a9330e1f0857434321b7a2c2482d84
yangyipeng001 <892973319@qq.com>
2022-12-13 22:41:11 +0800

    'feat:shader着色器编写高级图案2'

--- src/shaders/deep/fragment.glsl
+++ src/shaders/deep/fragment.glsl
@@ -1,6 +1,7 @@
 precision lowp float;
 
 uniform float uTime;
+uniform float uScale;
 
 varying vec2 vUv;
 
@@ -18,6 +19,68 @@ vec2 rotate(vec2 uv, float rotation, vec2 mid) {
         cos(rotation) * (uv.x - mid.x) + sin(rotation) * (uv.y - mid.y) + mid.x,
         cos(rotation) * (uv.y - mid.y) - sin(rotation) * (uv.x - mid.x) + mid.y
     );
+}
+
+// 噪声函数
+float noise(in vec2 _st) {
+    vec2 i = floor(_st);
+    vec2 f = fract(_st);
+
+    // Four corners in 2D of a tile
+    float a = random(i);
+    float b = random(i + vec2(1.0, 0.0));
+    float c = random(i + vec2(0.0, 1.0));
+    float d = random(i + vec2(1.0, 1.0));
+
+    vec2 u = f * f * (3.0 - 2.0 * f);
+
+    return mix(a, b, u.x) + (c - a) * u.y * (1.0 - u.x) + (d - b) * u.x * u.y;
+}
+
+//	Classic Perlin 2D Noise 
+//	by Stefan Gustavson
+//
+vec4 permute(vec4 x)
+{
+    return mod(((x*34.0)+1.0)*x, 289.0);
+}
+
+vec2 fade(vec2 t)
+{
+    return t*t*t*(t*(t*6.0-15.0)+10.0);
+}
+
+float cnoise(vec2 P)
+{
+    vec4 Pi = floor(P.xyxy) + vec4(0.0, 0.0, 1.0, 1.0);
+    vec4 Pf = fract(P.xyxy) - vec4(0.0, 0.0, 1.0, 1.0);
+    Pi = mod(Pi, 289.0); // To avoid truncation effects in permutation
+    vec4 ix = Pi.xzxz;
+    vec4 iy = Pi.yyww;
+    vec4 fx = Pf.xzxz;
+    vec4 fy = Pf.yyww;
+    vec4 i = permute(permute(ix) + iy);
+    vec4 gx = 2.0 * fract(i * 0.0243902439) - 1.0; // 1/41 = 0.024...
+    vec4 gy = abs(gx) - 0.5;
+    vec4 tx = floor(gx + 0.5);
+    gx = gx - tx;
+    vec2 g00 = vec2(gx.x,gy.x);
+    vec2 g10 = vec2(gx.y,gy.y);
+    vec2 g01 = vec2(gx.z,gy.z);
+    vec2 g11 = vec2(gx.w,gy.w);
+    vec4 norm = 1.79284291400159 - 0.85373472095314 * vec4(dot(g00, g00), dot(g01, g01), dot(g10, g10), dot(g11, g11));
+    g00 *= norm.x;
+    g01 *= norm.y;
+    g10 *= norm.z;
+    g11 *= norm.w;
+    float n00 = dot(g00, vec2(fx.x, fy.x));
+    float n10 = dot(g10, vec2(fx.y, fy.y));
+    float n01 = dot(g01, vec2(fx.z, fy.z));
+    float n11 = dot(g11, vec2(fx.w, fy.w));
+    vec2 fade_xy = fade(Pf.xy);
+    vec2 n_x = mix(vec2(n00, n01), vec2(n10, n11), fade_xy.x);
+    float n_xy = mix(n_x.x, n_x.y, fade_xy.y);
+    return 2.3 * n_xy;
 }
 
 void main(){
@@ -250,7 +313,44 @@ void main(){
     // gl_FragColor = vec4(strength, strength, strength, 1);
 
     // 45. 光芒四射
-    float angle = atan(vUv.x - 0.5, vUv.y - 0.5) / (PI * 2.0);
-    float strength = sin(angle * 100.0);
-    gl_FragColor = vec4(strength, strength, strength, 1);
+    // float angle = atan(vUv.x - 0.5, vUv.y - 0.5) / (PI * 2.0);
+    // float strength = sin(angle * 100.0);
+    // gl_FragColor = vec4(strength, strength, strength, 1);
+
+    // 46. 使用噪声函数实现烟雾、波纹效果
+    // float strength = noise(vUv);
+    // gl_FragColor = vec4(strength, strength, strength, 1);
+
+    // float strength = noise(vUv * 10.0);
+    // gl_FragColor = vec4(strength, strength, strength, 1);
+
+    // float strength = step(0.5, noise(vUv * 10.0));
+    // gl_FragColor = vec4(strength, strength, strength, 1);
+
+    // 通过时间设置波形
+    // float strength = step(uScale, cnoise(vUv * 10.0 + uTime));
+    // gl_FragColor = vec4(strength, strength, strength, 1);
+
+    // float strength = abs(cnoise(vUv * 10.0));
+    // gl_FragColor = vec4(strength, strength, strength, 1);
+
+    // 发光路径
+    // float strength = 1.0 - abs(cnoise(vUv * 10.0));
+    // gl_FragColor = vec4(strength, strength, strength, 1);
+
+    // 波纹效果
+    // float strength = sin(cnoise(vUv * 10.0) * 5.0 + uTime);
+    // gl_FragColor = vec4(strength, strength, strength, 1);
+
+    // float strength = step(0.9, sin(cnoise(vUv * 10.0) * 20.0));
+    // gl_FragColor = vec4(strength, strength, strength, 1);
+
+    // 使用混合函数混颜色
+    vec3 purpleColor = vec3(1.0, 0.0, 1.0);
+    vec3 greenColor = vec3(0.0, 1.0, 0.0);
+    vec3 uvColor = vec3(vUv, 1.0);
+    float strength = step(0.9, sin(cnoise(vUv * 10.0) * 20.0));
+    vec3 mixColor = mix(uvColor, greenColor, strength);
+    // gl_FragColor = vec4(mixColor, 1.0);
+    gl_FragColor = vec4(mixColor, 1.0);
 }

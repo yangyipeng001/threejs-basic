c973da175409d9e77b9ef9e27e300e8cc81a4235
yangyipeng001 <892973319@qq.com>
2023-01-08 23:47:39 +0800

    'feat:shader着色器打造漫天飞舞孔明灯-1'

---
+++ src/main/flyLight/main.js
@@ -0,0 +1,110 @@
+import * as THREE from "three";
+
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import gsap from "gsap";
+import * as dat from "dat.gui";
+import vertexShader from "../../shaders/flyLight/vertex.glsl";
+import fragmentShader from "../../shaders/flyLight/fragment.glsl";
+import {RGBELoader} from 'three/examples/jsm/loaders/RGBELoader'
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
+
+// 目标：实现孔明灯
+
+// 创建gui对象
+const gui = new dat.GUI();
+
+// console.log(THREE);
+// 初始化场景
+const scene = new THREE.Scene();
+
+// 创建透视相机
+const camera = new THREE.PerspectiveCamera(
+  90,
+  window.innerHeight / window.innerHeight,
+  0.1,
+  1000
+);
+// 设置相机位置
+// object3d具有position，属性是1个3维的向量
+camera.position.set(0, 0, 2);
+// 更新摄像头
+camera.aspect = window.innerWidth / window.innerHeight;
+//   更新摄像机的投影矩阵
+camera.updateProjectionMatrix();
+scene.add(camera);
+
+// 加入辅助轴，帮助我们查看3维坐标轴
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+
+// 加载纹理
+// 创建环境纹理
+const rgbeLoader = new RGBELoader()
+// 因为图片有点大，所以异步加载
+rgbeLoader.loadAsync('./assets/2k.hdr').then((texture) => {
+    // 按照圆柱体映射
+    texture.mapping = THREE.EquirectangularReflectionMapping
+
+    scene.background = texture
+    scene.environment = texture
+})
+
+
+// 创建着色器材质;
+const shaderMaterial = new THREE.ShaderMaterial({
+  vertexShader: vertexShader,
+  fragmentShader: fragmentShader,
+  uniforms: {},
+  side: THREE.DoubleSide,
+  transparent: true,
+});
+
+
+// 初始化渲染器
+const renderer = new THREE.WebGLRenderer({ alpha: true });
+// renderer.shadowMap.enabled = true;
+// renderer.shadowMap.type = THREE.BasicShadowMap;
+// renderer.shadowMap.type = THREE.VSMShadowMap;
+// 渲染器的输出编码
+renderer.outputEncoding = THREE.sRGBEncoding
+// 色调映射
+
+// 设置渲染尺寸大小
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+// 监听屏幕大小改变的变化，设置渲染的尺寸
+window.addEventListener("resize", () => {
+  //   console.log("resize");
+  // 更新摄像头
+  camera.aspect = window.innerWidth / window.innerHeight;
+  //   更新摄像机的投影矩阵
+  camera.updateProjectionMatrix();
+
+  //   更新渲染器
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  //   设置渲染器的像素比例
+  renderer.setPixelRatio(window.devicePixelRatio);
+});
+
+// 将渲染器添加到body
+document.body.appendChild(renderer.domElement);
+
+// 初始化控制器
+const controls = new OrbitControls(camera, renderer.domElement);
+// 设置控制器阻尼
+controls.enableDamping = true;
+// 设置自动旋转
+// controls.autoRotate = true;
+
+const clock = new THREE.Clock();
+function animate(t) {
+  const elapsedTime = clock.getElapsedTime();
+
+  //   console.log(elapsedTime);
+  requestAnimationFrame(animate);
+  // 使用渲染器渲染相机看这个场景的内容渲染出来
+  renderer.render(scene, camera);
+}
+
+animate();

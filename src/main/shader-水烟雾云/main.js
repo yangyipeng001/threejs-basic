dfdf5dc980f69a8be9612eaab702141207c97ace
yangyipeng001 <892973319@qq.com>
2023-01-10 23:36:39 +0800

    'feat:着色器打造水云烟雾-代码准备'

---
+++ src/main/shader-水烟雾云/main.js
@@ -0,0 +1,106 @@
+import * as THREE from "three";
+
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import gsap from "gsap";
+import * as dat from "dat.gui";
+import vertexShader from "../../shaders/water/vertex.glsl";
+import fragmentShader from "../../shaders/water/fragment.glsl";
+import {RGBELoader} from 'three/examples/jsm/loaders/RGBELoader'
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
+
+// 目标：实现孔明灯
+
+// 创建gui对象
+const gui = new dat.GUI();
+
+// console.log(THREE);
+// 初始化场景
+const scene = new THREE.Scene();
+
+// 创建透视相机
+const camera = new THREE.PerspectiveCamera(
+  90,
+  window.innerHeight / window.innerHeight,
+  0.1,
+  1000
+);
+// 设置相机位置
+// object3d具有position，属性是1个3维的向量
+camera.position.set(0, 0, 2);
+// 更新摄像头
+camera.aspect = window.innerWidth / window.innerHeight;
+// 更新摄像机的投影矩阵
+camera.updateProjectionMatrix();
+scene.add(camera);
+
+// 加入辅助轴，帮助我们查看3维坐标轴
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+// 创建着色器材质;
+const shaderMaterial = new THREE.ShaderMaterial({
+  vertexShader: vertexShader,
+  fragmentShader: fragmentShader,
+});
+
+const plane = new THREE.Mesh(
+  new THREE.PlaneBufferGeometry(1, 1, 512, 512),
+  shaderMaterial
+)
+plane.rotation.x = -Math.PI / 2
+scene.add(plane)
+
+
+// 初始化渲染器
+const renderer = new THREE.WebGLRenderer({ alpha: true });
+// renderer.shadowMap.enabled = true;
+// renderer.shadowMap.type = THREE.BasicShadowMap;
+// renderer.shadowMap.type = THREE.VSMShadowMap;
+// 渲染器的输出编码
+// renderer.outputEncoding = THREE.sRGBEncoding
+// 色调映射
+// 这个属性用于在普通计算机显示器或者移动设备屏幕等低动态范围介质上，模拟、逼近高动态范围（HDR）效果。
+// renderer.toneMapping = THREE.ACESFilmicToneMapping
+// 线性级别
+// renderer.toneMapping = THREE.LinearToneMapping
+// renderer.toneMapping = THREE.ReinhardToneMapping
+// renderer.toneMapping = THREE.CineonToneMapping
+
+// 色调映射的曝光级别。默认是1
+// renderer.toneMappingExposure = 0.2
+
+
+// 设置渲染尺寸大小
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+// 监听屏幕大小改变的变化，设置渲染的尺寸
+window.addEventListener("resize", () => {
+  //   console.log("resize");
+  // 更新摄像头
+  camera.aspect = window.innerWidth / window.innerHeight;
+  //   更新摄像机的投影矩阵
+  camera.updateProjectionMatrix();
+
+  //   更新渲染器
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  //   设置渲染器的像素比例
+  renderer.setPixelRatio(window.devicePixelRatio);
+});
+
+// 将渲染器添加到body
+document.body.appendChild(renderer.domElement);
+
+// 初始化控制器
+const controls = new OrbitControls(camera, renderer.domElement);
+// 设置控制器阻尼
+controls.enableDamping = true;
+
+const clock = new THREE.Clock();
+function animate(t) {
+  const elapsedTime = clock.getElapsedTime();
+  requestAnimationFrame(animate);
+  // 使用渲染器渲染相机看这个场景的内容渲染出来
+  renderer.render(scene, camera);
+}
+
+animate();
